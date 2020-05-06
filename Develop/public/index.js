f94084ba285ebe44ae70c3f391a07b61a8e8d8a3
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-06 16:23:10 -0500

    added if route to index.js

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -5,9 +5,4 @@ async function init() {
     const workout = await API.getLastWorkout();
     if (workout) {
       location.search = "?id=" + workout._id;
-    } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
-    }
-  }
-}
-
+    } 
